5813332abb8d51c307b9b16a64a40f986d133290
toshiboufishing-hue <toshibou.fishing@gmail.com>
2026-09-08 22:41:27 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='stock-buddy-v055';
+const CACHE='stock-buddy-v056';
 const ASSETS=['./','./index.html','./styles.css?v=0.55.0','./app.js?v=0.55.0','./manifest.webmanifest'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
